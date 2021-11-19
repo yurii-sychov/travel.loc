b38c5015bf9c5ecb8d2b7491508e83830d489dfa
Yurii Sychov <yurii@sychov.pp.ua>
2021-11-19 08:18:17 +0200

    2021-11-19_08:18

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 				<section className='navbar'>
 					<nav className='navbar navbar-expand-lg navbar-light bg-light'>
 						<div className='container-fluid'>
-							<a className='navbar-brand' href='#'>
+							<a className='navbar-brand' href='/'>
 								Navbar
 							</a>
 							<button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
@@ -19,27 +19,27 @@ function App() {
 							<div className='collapse navbar-collapse' id='navbarSupportedContent'>
 								<ul className='navbar-nav me-auto mb-2 mb-lg-0'>
 									<li className='nav-item'>
-										<a className='nav-link active' aria-current='page' href='#'>
+										<a className='nav-link active' aria-current='page' href='/'>
 											Home
 										</a>
 									</li>
 									<li className='nav-item'>
-										<a className='nav-link' href='#'>
+										<a className='nav-link' href='/'>
 											Link
 										</a>
 									</li>
 									<li className='nav-item dropdown'>
-										<a className='nav-link dropdown-toggle' href='#' id='navbarDropdown' role='button' data-bs-toggle='dropdown' aria-expanded='false'>
+										<a className='nav-link dropdown-toggle' href='/' id='navbarDropdown' role='button' data-bs-toggle='dropdown' aria-expanded='false'>
 											Dropdown
 										</a>
 										<ul className='dropdown-menu' aria-labelledby='navbarDropdown'>
 											<li>
-												<a className='dropdown-item' href='#'>
+												<a className='dropdown-item' href='/'>
 													Action
 												</a>
 											</li>
 											<li>
-												<a className='dropdown-item' href='#'>
+												<a className='dropdown-item' href='/'>
 													Another action
 												</a>
 											</li>
@@ -47,14 +47,14 @@ function App() {
 												<hr className='dropdown-divider' />
 											</li>
 											<li>
-												<a className='dropdown-item' href='#'>
+												<a className='dropdown-item' href='/'>
 													Something else here
 												</a>
 											</li>
 										</ul>
 									</li>
 									<li className='nav-item'>
-										<a className='nav-link disabled' href='#' tabindex='-1' aria-disabled='true'>
+										<a className='nav-link disabled' href='/' tabindex='-1' aria-disabled='true'>
 											Disabled
 										</a>
 									</li>
